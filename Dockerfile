3d7d67afbbbafeae63515d571caf110c1c50d7fc
Jarek Lipski <loomchild@loomchild.net>
2016-06-12 16:50:15 +0200

    Do not provide nginx.conf via mount, build image

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,8 @@
-FROM piwik
+FROM nginx
 
+COPY nginx.conf /etc/nginx/nginx.conf
 COPY docker-entrypoint.sh /entrypoint.sh
+
+ENTRYPOINT ["/entrypoint.sh"]
+CMD ["nginx", "-g", "daemon off;"]
+
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 set -e
 
-if [ ! -e piwik.php ]; then
-	tar cf - --one-file-system -C /usr/src/piwik . | tar xf -
-	chown -R www-data .
-
-    sed -i "s/'host'\s*=>\s*'127\.0\.0\.1',/'host'=>'db','username'=>'root','password'=>'piwik','dbname'=>'piwik',/" plugins/Installation/FormDatabaseSetup.php
-    sed -i "s/unset(\$config->General\['installation_in_progress'\]);/unset(\$config->General['installation_in_progress']);\$config->mail['transport'] = 'smtp';\$config->mail['host'] = 'mail';/" plugins/Installation/Controller.php
+if [ ! -e /var/www/puffin ]; then
+    sleep 2
+    sed -i "s/'host'\s*=>\s*'127\.0\.0\.1',/'host'=>'db','username'=>'root','password'=>'piwik','dbname'=>'piwik',/" /var/www/html/plugins/Installation/FormDatabaseSetup.php
+    sed -i "s/unset(\$config->General\['installation_in_progress'\]);/unset(\$config->General['installation_in_progress']);\$config->mail['transport'] = 'smtp';\$config->mail['host'] = 'mail';/" /var/www/html/plugins/Installation/Controller.php
+    touch /var/www/puffin
 fi
 
 exec "$@"
